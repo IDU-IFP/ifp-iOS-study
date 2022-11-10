89d987ef23692028b3f337855e9863e567cbff39
ryukyunghye <ryukyung123@naver.com>
2022-11-10 11:06:09 +0900

    :tada: iOS App project

---
+++ week2/week2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  week2
+//
+//  Created by 류경혜 on 2022/11/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ week2/week2/week2App.swift
@@ -0,0 +1,17 @@
+//
+//  week2App.swift
+//  week2
+//
+//  Created by 류경혜 on 2022/11/10.
+//
+
+import SwiftUI
+
+@main
+struct week2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
