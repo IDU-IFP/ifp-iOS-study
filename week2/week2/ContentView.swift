bb933c9e560343fd695c5d4fee608dc3934e0f2f
ryukyunghye <ryukyung123@naver.com>
2022-11-11 14:01:22 +0900

    :sparkles: 눌러서 쓰는 Button 사용해보기

--- week2/week2/ContentView.swift
+++ week2/week2/ContentView.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // 이벤트가 발생했을 때의 처리
+        Button {
+            print("Hi, cliked button")
+        // 버튼의 외형
+        } label: {
+            Image(systemName: "heart")
+            Text("Gaori")
+        }
+        .frame(width: 150, height: 50)
+        .font(.title)
+        .fontWeight(.bold)
+        .foregroundColor(Color.white)
+        .multilineTextAlignment(.center)
+    
+        .background(LinearGradient(gradient: Gradient(colors: [Color.yellow, Color.green]), startPoint: .leading, endPoint: .trailing))
+        .cornerRadius(15)
+
     }
 }
 
