b0b1c592e0b9c535586edfabe57159a487256aaf
leejihee <jihee6986@naver.com>
2022-11-17 00:47:33 +0900

    week3 commit test

---
+++ week3/week3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  week3
+//
+//  Created by 이지희 on 2022/11/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ week3/week3/week3App.swift
@@ -0,0 +1,17 @@
+//
+//  week3App.swift
+//  week3
+//
+//  Created by 이지희 on 2022/11/17.
+//
+
+import SwiftUI
+
+@main
+struct week3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
