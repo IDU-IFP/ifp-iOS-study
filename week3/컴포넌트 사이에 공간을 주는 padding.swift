e511cc1ee2bce8fe5085e1ec46cd09f9eb2b8b32
hyoeunla <96915074+hyoeunla@users.noreply.github.com>
2022-11-17 20:26:27 +0900

    컴포넌트 사이에 공간을 주는 padding

---
+++ week3/컴포넌트 사이에 공간을 주는 padding.swift
@@ -0,0 +1,18 @@
+VStack {
+            Image(systemName: "heart")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100)
+                .padding(.bottom, 100)
+                .background(.green)
+                .foregroundColor(.red)
+            
+            Image(systemName: "heart")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100)
+                .padding(.leading, 100)
+                .background(.green)
+                .foregroundColor(.red)
+                
+        }
