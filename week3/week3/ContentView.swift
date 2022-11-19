af7629c67d1961b26b980b3dabff6611e29d8c20
ryukyunghye <ryukyung123@naver.com>
2022-11-19 14:57:51 +0900

    :sparkles: 이제가지 배울걸로 간단하기 만들어보기

--- week3/week3/ContentView.swift
+++ week3/week3/ContentView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var change: Bool = false
+    
     var body: some View {
-        
+        ZStack{
+            Color.blue.edgesIgnoringSafeArea(.all)
+            VStack{
+                Spacer()
+                Image(systemName: change ? "fish.fill" : "fish")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200)
+                    .foregroundColor(.white)
+                Spacer()
+                HStack{
+                    Text("gaori")
+                        .foregroundColor(.white)
+                    
+                    Button {
+                        // true <-> false
+                        change.toggle()
+                    } label: {
+                        Text("Hi")
+                            .padding()
+                            .background(.white)
+                            .frame(width: 60, height: 60)
+                            .cornerRadius(50)
+                        
+                    }
+
+                }
+            }
+        }
+
     }
 }
 
