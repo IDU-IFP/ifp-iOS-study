2d48d467d92ec16c64d319052c802e806f718ef9
ryukyunghye <ryukyung123@naver.com>
2022-11-19 14:02:17 +0900

    :sparkles: UI를 그릴 때는 View

--- week3/week3/View/ViewContentView.swift
+++ week3/week3/View/ViewContentView.swift
@@ -5,4 +5,33 @@
 //  Created by 류경혜 on 2022/11/19.
 //
 
-import Foundation
+import SwiftUI
+
+struct ViewContentView: View {
+    var body: some View {
+        VStack{
+            Text("MyView")
+            Spacer()
+            MyView()
+        }
+        .padding(.vertical, 350)
+        .fontWeight(.bold)
+    }
+}
+struct MyView: View{
+    var body: some View{
+        Button {
+            
+        } label: {
+            Text("Hi gaori")
+                .foregroundColor(.green)
+        }
+
+    }
+}
+
+struct ViewContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewContentView()
+    }
+}
