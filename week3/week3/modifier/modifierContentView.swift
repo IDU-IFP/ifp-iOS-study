d8b0e3cbd1cd987979bdc0ef05bf2ce577bf30d9
ryukyunghye <ryukyung123@naver.com>
2022-11-19 14:33:45 +0900

    :sparkles: View의 설정? 옵션? modifier에 대해 알아보자

--- week3/week3/modifier/modifierContentView.swift
+++ week3/week3/modifier/modifierContentView.swift
@@ -5,4 +5,20 @@
 //  Created by 류경혜 on 2022/11/19.
 //
 
-import Foundation
+import SwiftUI
+
+struct modifierContentView: View {
+    var body: some View {
+        Image(systemName: "house.fill")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 100)
+            .foregroundColor(.green)
+    }
+}
+
+struct modifierContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        modifierContentView()
+    }
+}
