086fd038d9f2133bd0866d1909c4eb792aeac32b
ryukyunghye <ryukyung123@naver.com>
2022-11-19 14:39:33 +0900

    :sparkles: 컴포넌트 사이에 공간을 주는 padding

--- week3/week3/padding/paddingContentView.swift
+++ week3/week3/padding/paddingContentView.swift
@@ -5,4 +5,32 @@
 //  Created by 류경혜 on 2022/11/19.
 //
 
-import Foundation
+import SwiftUI
+
+struct paddingContentView: View {
+    var body: some View {
+        VStack{
+            Image(systemName: "house.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100)
+                .foregroundColor(.green)
+                .padding([.bottom, .top], 100)
+                .background(.black)
+            Image(systemName: "house.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100)
+                .foregroundColor(.green)
+                .padding(.leading, 100)
+                .background(.black)
+        }
+    }
+
+}
+
+struct paddingContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        paddingContentView()
+    }
+}
