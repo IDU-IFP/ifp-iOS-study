441d6163e4554d3d4700c17b6a30718689f71f62
ryukyunghye <ryukyung123@naver.com>
2022-11-19 13:51:57 +0900

    :sparkles: 컴포넌트 사이의 공간을 만들어주는 Spacer

--- week3/week3/Spacer/SpacerContentView.swift
+++ week3/week3/Spacer/SpacerContentView.swift
@@ -5,4 +5,35 @@
 //  Created by 류경혜 on 2022/11/19.
 //
 
-import Foundation
+import SwiftUI
+
+struct SpacerContentView: View {
+    var body: some View {
+        VStack{
+            HStack{
+                Image(systemName: "arrowshape.turn.up.right.fill")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .aspectRatio(contentMode: .fit)
+                Text("gaori")
+                    .fontWeight(.bold)
+            }
+            .foregroundColor(.green)
+            Spacer()
+            HStack{
+                Image(systemName: "heart")
+                Spacer()
+                Text("gaori")
+            }.padding(.horizontal, 150)
+                .foregroundColor(.red)
+                .fontWeight(.bold)
+        }
+
+    }
+}
+
+struct SpacerContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        SpacerContentView()
+    }
+}
