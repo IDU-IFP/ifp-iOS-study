85b19ace1cb3875e3b6aedc1f3fb59eb22031996
ryukyunghye <ryukyung123@naver.com>
2022-11-19 14:16:53 +0900

    :sparkles: 앱 화면을 다시 그리기 위한 상태 State

--- week3/week3/State/StateContentView.swift
+++ week3/week3/State/StateContentView.swift
@@ -5,4 +5,26 @@
 //  Created by 류경혜 on 2022/11/19.
 //
 
-import Foundation
+import SwiftUI
+
+struct StateContentView: View {
+    // 구조체 안에 있는 변수: @State를 제거할 수 없다
+    @State var name: String = ""
+    var body: some View {
+        VStack{
+            Text("Hi \(name)")
+            Button {
+                name = "gaori"
+            } label: {
+                Text("Click")
+            }
+
+        }
+    }
+}
+
+struct StateContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        StateContentView()
+    }
+}
