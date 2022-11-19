6e0315744240482282e8ee5dd2e03a5c60488339
ryukyunghye <ryukyung123@naver.com>
2022-11-19 13:10:59 +0900

    :sparkles: 색상을 그려주는 Color

--- week3/week3/Color/ColorContentView.swift
+++ week3/week3/Color/ColorContentView.swift
@@ -5,4 +5,21 @@
 //  Created by 류경혜 on 2022/11/19.
 //
 
-import Foundation
+import SwiftUI
+
+struct ColorContentView: View {
+    var body: some View {
+        VStack{
+            Color(.orange)
+            Color(.yellow).edgesIgnoringSafeArea(.top)
+            Color(.gray).edgesIgnoringSafeArea(.bottom)
+            Color(.lightGray).edgesIgnoringSafeArea(.all)
+        }
+    }
+}
+
+struct ColorContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
