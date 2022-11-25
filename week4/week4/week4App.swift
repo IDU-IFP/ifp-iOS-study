b8407623d832716f19e26599ed819b5f653af9f2
ryukyunghye <ryukyung123@naver.com>
2022-11-26 02:11:47 +0900

    :tada: week4 project

---
+++ week4/week4/week4App.swift
@@ -0,0 +1,17 @@
+//
+//  week4App.swift
+//  week4
+//
+//  Created by 류경혜 on 2022/11/26.
+//
+
+import SwiftUI
+
+@main
+struct week4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
