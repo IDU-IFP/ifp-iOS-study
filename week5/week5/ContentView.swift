99c9aedab21ad1d2b9ac40e862aad40b3889370f
ryukyunghye <ryukyung123@naver.com>
2022-12-03 14:38:59 +0900

    :tada: week5 project

---
+++ week5/week5/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  week5
+//
+//  Created by 류경혜 on 2022/12/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
