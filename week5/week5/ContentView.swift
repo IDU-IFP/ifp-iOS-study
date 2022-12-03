c2a7e8796da1a610a531a4a451df9ca5b7676753
ryukyunghye <ryukyung123@naver.com>
2022-12-03 16:14:19 +0900

    :sparkles: 이번 강의에서 배운 것을 정리해 봅시다

--- week5/week5/ContentView.swift
+++ week5/week5/ContentView.swift
@@ -8,13 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var userID: String = ""
+    @State var userPW: String = ""
+    @State var logIn: Bool = false
+    @State var ShownPW: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            HStack{
+                Label{
+                    Text("ID: ")
+                } icon:{
+                    Image(systemName: "person.fill")
+                }
+                TextField("enter ID", text: $userID)
+            }
+            Divider()
+            HStack{
+                Label{
+                    Text("PW: ")
+                } icon:{
+                    Image(systemName: "lock.fill")
+                }
+                if ShownPW{
+                    TextField("enter PW", text: $userPW)
+                } else{
+                    SecureField("enter PW", text: $userPW)
+                }
+                Spacer()
+                Toggle(isOn: $ShownPW){
+                    Text("Show")
+                }
+            }
+            Button{
+                if userPW == "1234"{
+                    logIn = true
+                } else{
+                    logIn = false
+                }
+            } label:{
+                Text("Sign in")
+                    .font(.body)
+                    .fontWeight(.bold)
+                    .padding()
+                    .foregroundColor(.green)
+            }
         }
+        .padding()
+        .sheet(isPresented: $logIn, content:{ Text("Hello gaori")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.green)
+            
+        })
         .padding()
     }
 }
